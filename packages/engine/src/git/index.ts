7991e9dcaa3371717a17281353d77b61d804b4ba
Taha <taha.saleem092@gmail.com>
2026-08-21 16:57:45 +0800

    feat(engine): git service with hidden-ref checkpoints

---
+++ packages/engine/src/git/index.ts
@@ -0,0 +1,10 @@
+export { DIFF_MAX_BYTES, GitService } from './git-service'
+export type {
+  CheckpointInfo,
+  GitError,
+  GitErrorCode,
+  GitServiceOptions,
+  GitStatus,
+  StatusEntry,
+  StatusKind,
+} from './git-service'
